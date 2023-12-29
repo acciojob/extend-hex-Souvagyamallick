84e7f7a5f1ab65e744dbf757efd8ea2bae4816ce
Souvagyamallick <140335318+Souvagyamallick@users.noreply.github.com>
2023-12-29 19:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let n=shortHex.length;
+	let i=n-3, isCapital = false;
+	let output = "#";
+	while(i<n){
+		let asciiCode = shotHex[i].charCodeAt(0);
+		if(asciiCode >= 65 && asciiCode <= 90){
+			isCapital =true;
+		}
+		output +=shotHex[i] + shotHex[i];
+		i++;
+	}
+
+	retrurn isCapital ? output.toUpperCase() : output;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+ const shortHex = prompt("Enter Short Hex.");
+ alert(extendHex(shortHex));
